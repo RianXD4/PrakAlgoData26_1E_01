bcdb7d59dc8890004b54fc323c2e118999f262bb
Rian <adriansanda003@gmail.com>
2026-04-02 22:48:53 +0700

    P6_Praktek02

---
+++ Pertemuan 6/src/MahasiswaBerprestasi01.java
@@ -0,0 +1,32 @@
+public class MahasiswaBerprestasi01 {
+    Mahasiswa01 [] listMhs = new Mahasiswa01[5];
+    int idx;
+
+    void tambah (Mahasiswa01 m){
+        if (idx<listMhs.length) {
+            listMhs[idx]=m;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh");
+        }
+    }
+
+    void tampil (){
+        for (Mahasiswa01 m : listMhs) {
+            m.tampilInformasi();
+            System.out.println("---------------------------------------");
+        }
+    }
+
+    void bubblesor(){
+        for (int i = 0; i < listMhs.length; i++) {
+            for (int j = 1; j < listMhs.length; j++) {
+                if (listMhs[j].ipk>listMhs[j-1].ipk) {
+                    Mahasiswa01 tmp = listMhs[j];
+                    listMhs[j]=listMhs[j-1];
+                    listMhs[j-1]=tmp;
+                }
+            }
+        }
+    }
+}
